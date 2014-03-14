6e3bcfe1a083fcc6477f6c1f8b3e26e6c44c32a8
GEOFRAME <riccardo.rigon@ing.unitn.it>
2014-03-14 14:42:00 +0100

    prova e riprova ci si riesce ... ma riusciremo a ripeterlo ?

---
+++ src/HelloAgain/HelloFrancesco.java
@@ -0,0 +1,10 @@
+package HelloAgain;
+
+public class HelloFrancesco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Ce la faremo");
+	}
+
+}
